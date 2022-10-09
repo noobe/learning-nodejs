fd34d0e34963c3206b2173fc5520d35bf633fce8
Sarath <sarath.regedit@gmail.com>
2022-10-09 23:44:17 +0530

    node http 2

--- 07 - http/07-b.js
+++ 07 - http/07-b.js
@@ -1,6 +1,12 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer(function(req, res){
+  try {
+    console.log(url.parse(req.url, true));
+  } catch(e){
+    console.log(e);
+  }
   res.writeHead(200, {'content-Type': 'text/html'});
   res.write(`<h1>Hello world</h1>`);
   res.end();
