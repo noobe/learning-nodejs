9b270bba21b2bcb250951765883de48b877ffa10
Sarath <sarath.regedit@gmail.com>
2022-10-13 23:39:20 +0530

    node js learning

---
+++ 09 - Sockets/09-a.js
@@ -0,0 +1,24 @@
+const { Server } = require('ws');
+ 
+const sockserver = new Server({ port: 443 });
+sockserver.on('connection', (ws) => {
+   console.log('New client connected!'); 
+   ws.on('close', () => console.log('Client has disconnected!'));
+});
+ 
+setInterval(() => {
+   sockserver.clients.forEach((client) => {
+       const data = JSON.stringify({'type': 'time', 'time': new Date().toTimeString()});
+       client.send(data);
+   });
+}, 1000);
+ 
+setInterval(() => {
+   sockserver.clients.forEach((client) => {
+       const messages = ['Hello', 'What do you ponder?', 'Thank you for your time', 'Be Mindful', 'Thank You'];
+       const random = Math.floor(Math.random() * messages.length);
+       let position = {x: Math.floor(Math.random() * 200), y: Math.floor(Math.random() * 150)}
+       const data = JSON.stringify({'type': 'message', 'message': messages[random], 'position': position});
+       client.send(data);
+   });
+}, 8000);
